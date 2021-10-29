fc96c09279e92460032e5b3b363d5f94923bef98
Bruno Codinha <bruno.codinha@tecnico.ulisboa.pt>
2021-10-29 13:35:37 +0100

    asd

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
 import os
 import sys
-
+sys.path.insert(1, '../python-qrcode')
+import qrcode
+from PIL import Image
+import PIL
 
 def main():
+    global url
+    global root_folder
     if len(sys.argv) < 2:
         print("Invalid arguments! Use python3 <name>")
 
@@ -21,7 +26,21 @@ def main():
 
         except OSError:
             pass
+	url = 'https://www.codinha.dev/' + root_folder + '/pages/menu.html'
 
+def createQRCode():
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img = img.save(root_folder + "_qrcode.png")
 
 if __name__ == "__main__":
     main()
+    createQRCode()
